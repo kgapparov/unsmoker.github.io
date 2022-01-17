25e3a293e3c5c0cd8ebdebbdbd29d9b987956ed2
khassangali <hasangaly@gmail.com>
2022-01-17 01:37:15 -0600

    add decorateMyText task solutions

---
+++ decoratemytext.js
@@ -0,0 +1,30 @@
+window.onload = function() {
+    var button = document.getElementById("btn");
+    button.onclick = myAlert;
+    var check = document.getElementById("check");
+    check.onchange = setNewStyle;
+}
+function myAlert(){
+    setInterval(increaseFontSize, 500);
+}
+
+function increaseFontSize() {
+    let text = document.getElementById("text");
+    var size  = getComputedStyle(text).fontSize;
+    text.style.fontSize = (parseInt(size) + 2) + "pt";
+}
+
+function setNewStyle(){
+    var checkbox = document.getElementById("check");
+    if (checkbox.checked) {
+        document.getElementById("text").style.fontWeight="bold";
+        document.getElementById("text").style.color="green";
+        document.getElementById("text").style.textDecoration="underline";
+
+        //this urls is not accessible, but added just for marking. 
+        
+        document.body.style.backgroundImage="url('http://www.cs.washington.edu/education/courses/190m/CurrentQtr/labs/6/hundred-dollar-bill.jpg')";
+    } else {
+        document.getElementById("text").style.fontWeight="400";
+    }
+}
