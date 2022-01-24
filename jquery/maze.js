9a0f15c463084952ed48ecb363d5515cb1ae4340
khassangali <hasangaly@gmail.com>
2022-01-24 02:10:30 -0600

    mazeTask

---
+++ jquery/maze.js
@@ -0,0 +1,49 @@
+$(document).ready(function () {
+   var gameStarted = false;
+   var colored = false; 
+   var result = true;
+   $(".boundary").hover(function (e) {
+        e.preventDefault();
+        e.stopImmediatePropagation();
+        setColor();     
+       }
+   );
+   $("#end").hover(function (e) {
+        e.preventDefault();
+        e.stopImmediatePropagation();
+        getResult() 
+   }
+   );
+   $(document).bind("keypress", function (e) {
+        e.preventDefault();
+        e.stopImmediatePropagation();
+        if (e.which == 115) {
+            startEndGame()
+        }
+   });
+
+   function setColor(){
+        if (gameStarted && !colored) {
+            $(".boundary").addClass("youlose");
+            result = false;
+            colored = true;
+        }
+   }
+
+   function startEndGame() {
+        if (gameStarted) {
+            gameStarted = false;
+        } else {
+            gameStarted = true;
+        }
+   }
+   function getResult() {
+        if (gameStarted && result) {
+            alert("You win! :] ");
+            gameStarted = false;
+        } else if (gameStarted && !result) {
+            alert ("You lost!");
+            gameStarted = false;
+        }
+   }
+});
